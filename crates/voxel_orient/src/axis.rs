549d3e6e6f4c17f168ea2fdd96b8dcc46189ed9c
ErisianArchitect <1135547+ErisianArchitect@users.noreply.github.com>
2026-05-08 11:26:56 -0700

    Documentation for `Axis`.

--- crates/voxel_orient/src/axis.rs
+++ crates/voxel_orient/src/axis.rs
@@ -2,6 +2,7 @@
 
 use crate::{Flip, direction::Direction, polarity::Pol};
 
+/// Represents a coordinate axis (either X, Y, or Z).
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[repr(u8)]
 pub enum Axis {
@@ -11,6 +12,7 @@ pub enum Axis {
 }
 
 impl Axis {
+    /// The direction on this axis in the positive direction.
     #[inline]
     pub const fn pos(self) -> Direction {
         match self {
@@ -20,6 +22,7 @@ impl Axis {
         }
     }
 
+    /// The direction on this axis in the negative direction.
     #[inline]
     pub const fn neg(self) -> Direction {
         match self {
@@ -29,6 +32,7 @@ impl Axis {
         }
     }
 
+    /// If `flipped` is `true`, this will return a [Flip] with the corresponding axis inverted.
     #[must_use]
     #[inline(always)]
     pub const fn as_flip_if(self, flipped: bool) -> Flip {
@@ -39,19 +43,32 @@ impl Axis {
         FLIPS[(self as usize) << (flipped as u32)]
     }
 
+    /// Returns a [Flip] with the corresponding axis inverted.
     #[must_use]
     #[inline(always)]
     pub const fn as_flip(self) -> Flip {
+        const _SAFETY: () = {
+            isit::assert_same_size_align::<Flip, u8>();
+            isit::const_assert(1 << (Axis::X as u8) == Flip::X.as_u8());
+            isit::const_assert(1 << (Axis::Y as u8) == Flip::Y.as_u8());
+            isit::const_assert(1 << (Axis::Z as u8) == Flip::Z.as_u8());
+        };
+        // SAFETY: Flip is represented using the first 3 bits of the byte.
+        //         Axis is represented as `X = 0, Y = 1, Z = 2`.
+        //         The index of the bits in `Flip` correspond to the discriminant value of `Axis.`
+        //         If the program compiles (the above checks passed), then this code is sound.
         unsafe {
             core::mem::transmute(1u8 << self as u32)
         }
     }
     
+    /// Create a [Direction] with the given polarity and the axis of `self`.
     #[inline]
     pub const fn with_polarity(self, pol: Pol) -> Direction {
         Direction::from_polar_axis(pol, self)
     }
 
+    /// Return a representation that can be displayed with a formatter.
     #[must_use]
     #[inline(always)]
     pub const fn display(self) -> AxisDisplay {
